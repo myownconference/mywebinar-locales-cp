a35f24c69e3508b016f5d338261d642097dd4936
Dmytro Pereuda <dmytro@pereuda.com>
2022-09-03 09:40:24 +0300

    проба ссылки в тултипе

--- en/admin.php
+++ en/admin.php
@@ -441,7 +441,7 @@ return [
 	'adhoc-webinars-title' => ////////////////////////////////////////////////////////////
 	//////////////////////////////////////////////////////////////////////////////////////
 	'
-		Permanent webinars <span class="tooltip" data-tip="These webinars are always available. You can use them without prior scheduling.<br/><br/>Any attendee can be invited by direct static link.<br/><br/>Password protection can be used to permit access."></span>
+		Permanent webinars <span class="tooltip" data-tip="These webinars are always available. You can use them without prior scheduling.<br/><br/>Any attendee can be invited by direct static link.<br/><br/>Password protection can be used to permit access.<br/><br/><a href=\"https://myownconference.com/blog/en/webinar-ad-hoc-the-fastest-way-to-start-a-meeting/\">Read more here</a>"></span>
 	',
 	//////////////////////////////////////////////////////////////////////////////////////
 	'webinars-title' => //////////////////////////////////////////////////////////////////

5a26ce86e9ba242a706e98deb5bb7dda54268124
Dmytro Pereuda <dmytro@pereuda.com>
2025-07-13 08:32:18 +0200

    added danish locale

---
+++ da/paid-webinars.php
@@ -0,0 +1,324 @@
+<?php
+
+// +-------------------------------------------------------------------+
+// | Copyright 2003-2023 MyOwnConference.com. All rights reserved.     |
+// |                                                                   |
+// | Unauthorized copying of this file, via any medium is strictly     |
+// | prohibited. Proprietary and confidential.                         |
+// |                                                                   |
+// | Written for myownconference.com <contact@myownconference.com>     |
+// +-------------------------------------------------------------------+
+
+
+return [
+	//////////////////////////////////////////////////////////////////////////////////////
+	'paypal-summ' => /////////////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////////////////////////
+	'
+		Allerede betalt
+	',
+	//////////////////////////////////////////////////////////////////////////////////////
+	'funds-received' => //////////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////////////////////////
+	'
+		Betalte webinarer <span class="tooltip" data-tip="Aktivér denne funktion for at kræve betaling fra deltagere for adgang til dine webinarer. Læs og bekræft vilkårene, før du aktiverer betalte webinarer i dine profilindstillinger. Bemærk, at funktionen ikke aktiveres med det samme, da vi manuelt gennemgår din profil og dine webinar-emner, før vi godkender det"></span>
+	',
+	//////////////////////////////////////////////////////////////////////////////////////
+	'hold-title' => //////////////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////////////////////////
+	'
+		Vores gebyr
+	',
+	//////////////////////////////////////////////////////////////////////////////////////
+	'output-title' => ////////////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////////////////////////
+	'
+		Behandlet
+	',
+	//////////////////////////////////////////////////////////////////////////////////////
+	'output-button' => ///////////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////////////////////////
+	'
+		Hæv beløb
+	',
+	//////////////////////////////////////////////////////////////////////////////////////
+	'payweb-rules-title' => //////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////////////////////////
+	'
+		Vilkår og betingelser for betalingshåndtering af betalte webinarer
+	',
+	//////////////////////////////////////////////////////////////////////////////////////
+	'payweb-rules-subtitle' => ///////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////////////////////////
+	'
+		Læs venligst disse vilkår og betingelser grundigt, før du bruger funktionen til betalingshåndtering. Forkert brug kan medføre automatisk blokering af din konto.
+	',
+	//////////////////////////////////////////////////////////////////////////////////////
+	'payweb-rules-bottom' => /////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////////////////////////
+	'
+		Underskriv her, hvis du accepterer vilkårene for betalingshåndtering
+	',
+	//////////////////////////////////////////////////////////////////////////////////////
+	'freeze-title' => ////////////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////////////////////////
+	'
+		På hold
+	',
+	//////////////////////////////////////////////////////////////////////////////////////
+	'service-commission-title' => ////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////////////////////////
+	'
+		Vores gebyr
+	',
+	//////////////////////////////////////////////////////////////////////////////////////
+	'our-bank-commission-title' => ///////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////////////////////////
+	'
+		Bankgebyr
+	',
+	//////////////////////////////////////////////////////////////////////////////////////
+	'available-title' => /////////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////////////////////////
+	'
+		Indtjening
+	',
+	//////////////////////////////////////////////////////////////////////////////////////
+	'extend-package-confirm' => //////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////////////////////////
+	'
+
+	',
+	//////////////////////////////////////////////////////////////////////////////////////
+	'withdraw-request-success' => ////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////////////////////////
+	'
+		
+	',
+	//////////////////////////////////////////////////////////////////////////////////////
+	'withdraw-request-error' => //////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////////////////////////
+	'
+		
+	',
+	//////////////////////////////////////////////////////////////////////////////////////
+
+
+	//////////////////////////////////////////////////////////////////////////////////////
+	'payweb-rules' => ////////////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////////////////////////
+	'
+		<h3>Håndtering af betalinger for dine betalte webinarer</h3>
+		<sup style="color:grey">(Senest redigeret: 24. august 2023. Revision: 1.2)</sup>
+		<br><br><br><br>
+		<h3>Vilkår og definitioner</h3>
+		<p>
+			– Betalte webinarer — omfatter webinarer, onlineforedrag, konferencer, seminarer, kurser, lektioner og livestreams, hvor deltagelse kræver betaling. Deltagere får adgang til disse webinarer, først når den nødvendige betaling er gennemført.
+			<br><br>
+			– Webinarsoftware MyOwnConference — vi er “Akovana, UAB“, en tjenesteudbyder specialiseret i afholdelse af både gratis og betalte webinarer, livestreams, webkonferencer og lignende. Vores registrerede adresse er Djioyi Street 18, Vilnius, Litauen, LT-01128.
+			<br><br>
+			– Bruger — enhver person, der har accepteret disse vilkår og anvender tjenesterne i overensstemmelse med denne aftale.
+			<br><br>
+			– Brugeraftale — dette dokument, som findes i kontrolpanelet, fungerer som en bindende kontrakt mellem dig (brugeren) og os (webstedet) og regulerer leveringen af vores informationstjenester.
+			<br><br>
+			– Brugerkonto, Kontrolpanel eller Dashboard — en samling af personlige oplysninger tilknyttet brugeren, som er nødvendige for at få adgang til de tjenester, webstedet tilbyder. Indeholder også de filer, som brugeren har gemt på webstedets server.
+			<br><br>
+			– Deltager, Gæst eller Bruger – en person inviteret af brugeren, som betaler brugeren for adgang til et betalt webinar, som brugeren har oprettet via webstedet.
+		</p>
+		<br><br>
+		<h3>Generelle bestemmelser</h3>
+		<p>
+			Når der afholdes betalte webinarer, fungerer platformen “MyOwnConference“, herefter kaldet “Platformen“, som mellemled. Platformen får af brugeren til opgave at modtage og behandle betalinger fra deltagere i webinarer, onlineforedrag, konferencer, kurser eller lektioner afholdt via platformens tjenester.
+		</p>
+		<br>
+		<p>
+			For at indgå en aftale med Platformen er det ikke nødvendigt at underskrive en fysisk kontrakt. Det er tilstrækkeligt at acceptere vilkårene i denne aftale ved at afkrydse det relevante felt i kontrolpanelet, når man aktiverer betalingstjenesten.
+		</p>
+		<br>
+		<p>
+			Platformen overvåger omhyggeligt kvaliteten af de betalte webinarer, håndterer klager fra deltagere og behandler tilbagebetalinger, hvis det er nødvendigt. Derfor forbeholder platformen sig retten til under den indledende gennemgang af en brugerkonto at afvise aktivering af betalingstjenester uden at angive årsag.
+		</p>
+		<br>
+		<p>
+			Alle betalinger modtages og behandles udelukkende via det internationale betalingssystem Paysera.
+		</p>
+		<br>
+		<p>
+			Den primære valuta, der anvendes til transaktioner, er euro (EUR).
+		</p>
+		<br>
+		<p>
+			Platformen overfører alle modtagne betalinger til en dedikeret konto, som udelukkende bruges til betalinger fra deltagerne i brugerens webinarer.
+		</p>
+		<br>
+		<p>
+			For at opretholde en komplet historik over kommunikationen med brugeren skal alle forespørgsler og klager vedrørende betalinger — herunder tilbagebetalingsanmodninger — sendes via den e-mailadresse, der er registreret på brugerens konto.
+		</p>
+		<br><br>
+		<h3>Platformgebyrer</h3>
+		<p>
+			Webstedet opkræver en provision på 10 % af hver gennemført betaling fra deltagere i brugerens betalte webinarer.
+		</p>
+		<p>
+			For eksempel, hvis deltagergebyret for et webinar er €10, og 10 personer deltager, vil den samlede betaling være €10 x 10 = €100. Efter fradrag af provisionen på 10 % modtager du €90.
+		</p>
+		<br><br>
+		<h3>Bankgebyrer</h3>
+		<p>
+			Et bankgebyr på €7 gælder kun ved overførsel til brugerens bankkonto. Dette gebyr opkræves konsekvent og uden undtagelser.
+		</p>
+		<br><br>
+		<h3>Korrespondentbankgebyrer</h3>
+		<p>
+			Disse gebyrer opkræves af korrespondentbanker i forbindelse med overførsler fra webstedets bankkonto til brugerens konto. Ved IBAN-overførsler kan gebyret variere fra €10 til €35. SEPA-overførsler er gebyrfri.
+		</p>
+		<br>
+		<br><br>
+		<h3>Udbetalinger</h3>
+		<p>
+			Når en deltager betaler for et webinar, sættes beløbet ind på en særskilt konto på platformen. Beløbene samles baseret på antal webinarer og deltagere. Udbetalinger behandles af platformen, men igangsættes kun efter brugerens anmodning via kontrolpanelet.
+		</p>
+		<br>
+		<p>
+			I henhold til denne aftale fratrækkes 10 % i provision samt €7 i bankgebyr fra det samlede udbetalingsbeløb.
+		</p>
+		<br>
+		<p>
+			Restbeløbet kan enten overføres til brugerens bankkonto eller anvendes som kredit til betaling for platformens tjenester.
+		</p>
+		<p>
+			Udbetalinger sker tidligst 7 arbejdsdage efter webinaret og kun hvis der ikke er indsendt nogen tilbagebetalingsanmodninger. Minimumsbeløbet for udbetaling er 300 EUR. For udbetalinger over 600 EUR sender platformen et afsluttende dokument, som skal underskrives af modtageren.
+		<br>
+		<br>
+		<p>
+			Vigtigt! Ved IBAN-overførsler kan visse mellemliggende banker opkræve et ekstra gebyr på mellem €10 og €35. Vi har ingen kontrol over disse gebyrer og kan ikke fjerne dem. Beløbet trækkes automatisk fra, når pengene overføres til brugerens konto.
+		</p>
+		<br><br>
+		<h3>Refusionsanmodning fra brugeren</h3>
+		<p>
+			Brugere kan anmode om refusion via kontrolpanelet på vores hjemmeside. Anmodningen skal bekræftes via den e-mailadresse, der er knyttet til brugerens konto.
+		</p>
+		<br>
+		<p>
+			For at opretholde en høj servicekvalitet behandles alle refusionsanmodninger fra deltagere i betalte webinarer delvist automatisk af vores system. Behandlingstiden for refusioner varierer mellem 1 og 7 hverdage og afhænger primært af webinar-deltagerens bank.
+		</p>
+		<br>
+		<p>
+			Tjenesten igangsætter refusionen straks efter modtagelse af brugerens anmodning.
+		</p>
+		<br><br>
+		<h3>Refusionsanmodning fra deltager</h3>
+		<p>
+			Vores platform behandler refusionsanmodninger fra deltagere i betalte webinarer kun, når de selv kontakter os via kontaktformularen eller den e-mailadresse, der er angivet på hjemmesiden.
+		</p>
+		<br>
+		<p>
+			I sådanne tilfælde vil vi udstede en refusion efter at have gennemgået deltagerens begrundelse og opnået aftale med kontohaveren (benævnt “Bruger“). Refusioner aktiveres ud fra meddelelser fra brugeren via kontrolpanelet.
+		</p>
+		<br>
+		<p>
+			Vi refunderer straks efter at have modtaget meddelelsen fra brugeren.
+		</p>
+		<br>
+		<p>
+			Hvis brugeren af en eller anden grund undlader at give os besked via kontrolpanelet, forbeholder platformen sig dog retten til at gennemføre refusionen til deltageren på egen hånd.
+		</p>
+		<br><br>
+		<h3>Refusionsanmodning fra deltagerens bank</h3>
+		<p>
+			Disse anmodninger stammer fra deltagerens bank i forbindelse med et betalt webinar. Alle betalinger fra deltagere til betalte webinarer kan refunderes fuldt ud i overensstemmelse med princippet: “Deltageren har altid ret“.
+		</p>
+		<br>
+		<p>
+			Hvis vi modtager en refusionsanmodning direkte fra deltagerens bank, informerer vi straks brugeren. Brugeren får derefter 3 hverdage til at tage kontakt til deltageren og meddele hjemmesiden, hvilken aftale der er opnået, ledsaget af relevant korrespondance og dokumentation. Disse dokumenter sendes derefter videre til den pågældende bank.
+		</p>
+		<br>
+		<p>
+			Bemærk: Hvis hjemmesiden modtager mere end 2 refusionsanmodninger fra den samme bank, der har udstedt deltagerens kort, forbeholder vi os retten til at deaktivere brugerens betalingsfunktion. Vi kan også tilbageføre tidligere gennemførte betalinger og om nødvendigt suspendere brugerens konto.
+		</p>
+		<br>
+		<p>
+			Derudover, hvis vi modtager mange klager over webinarernes indhold eller kvalitet, forbeholder vi os retten til at tilbagebetale alle deltagere uden undtagelse. I så fald vil brugeren ikke modtage de omstridte midler.
+		</p>
+		<br>
+		<p>
+			Hjemmesiden håndhæver denne regel konsekvent.
+		</p>
+		<br><br>
+		<h3>Hjemmesidens rettigheder og forpligtelser</h3>
+		<p>
+			Hjemmesiden tilstræber at sikre maksimal tilgængelighed og ydeevne, men kan ikke holdes ansvarlig for forsinkelser eller manglende opfyldelse af sine forpligtelser på grund af uforudsete omstændigheder uden for dens kontrol. Dette inkluderer, men er ikke begrænset til: dårlig internetforbindelse hos brugeren eller deltagerne under webinarer, livesendinger eller webkonferencer; mangel på udstyr som mikrofoner eller kameraer; utilstrækkelig ydeevne på brugerens enhed; DDoS-angreb på vores datacentre; naturkatastrofer; strejker; krigshandlinger; fejl eller forsinkelser i tredjepartstjenester og lignende.
+		</p>
+		<br>
+		<p>
+			Hjemmesiden kan ikke holdes ansvarlig for fejl, skader eller andre uforudsete hændelser, der skyldes brugerens eller deltagerens forkert anvendelse af tjenesten.
+		</p>
+		<br>
+		<p>
+			I henhold til sin <a href="https://myownconference.com/refund-policy/" target="_blank" rel="noopener noreferrer">refusionspolitik</a> fraskriver hjemmesiden sig ansvar for klager fra deltagere om dårlig organisering af betalte webinarer eller tekniske problemer, der skyldes brugeren.
+		</p>
+		<br>
+		<p>
+			Hjemmesiden forbeholder sig retten til at suspendere eller opsige sine tjenester uden forudgående varsel til brugeren. Dette inkluderer også muligheden for at tilbageholde alle indtægter fra betalte webinarer, især hvis indholdet viser sig at være ulovligt eller stødende.
+		</p>
+		<br><br>
+		<h3>Brugerens rettigheder og forpligtelser</h3>
+		<p>
+			Brugeren er personligt ansvarlig over for deltagerne i betalte webinarer for indholdet, kvaliteten og rettidig levering ved brug af de tjenester, som webstedet stiller til rådighed.
+		</p>
+		<br>
+		<p>
+			Brugeren påtager sig personligt ansvar over for deltagerne i betalte webinarer for eventuelle tekniske problemer, fejl, forsinkelser eller aflysninger i forbindelse med afholdelsen af webinaret.
+		</p>
+		<br>
+		<p>
+			Brugeren forpligter sig til ikke at benytte webstedets tjenester til ulovlige formål. Overtrædelse af denne aftale medfører øjeblikkelig suspension af brugerens konto, og eventuelle optjente midler fra betalte webinarer tilbageholdes af webstedet.
+		</p>
+		<br>
+		<p>
+			Brugeren må ikke anvende tjenesten eller dens beskedsystem til at sende, dele eller distribuere spam eller anden uautoriseret reklame. Hvis deltagere indsender klager over sådanne aktiviteter, forbeholder webstedet sig retten til at suspendere brugerens konto og tilbageholde alle indtjente midler fra betalte webinarer.
+		</p>
+		<br><br>
+		<h3>Ophavsretspolitik</h3>
+		<p>
+			Brugere må ikke offentliggøre eller gengive ophavsretligt beskyttet materiale eller varemærker på tjenesten uden forudgående tilladelse fra rettighedshaveren.
+		</p>
+		<br>
+		<p>
+			Hvis en bruger overtræder denne politik, forbeholder webstedet sig retten til at handle i overensstemmelse med afsnittet “Webstedets rettigheder og forpligtelser” i denne brugeraftale.
+		</p>
+		<br><br>
+		<h3>Ansvarsbegrænsning</h3>
+		<p>
+			Webstedet kan ikke holdes ansvarligt for skader, herunder tabt fortjeneste, som påføres deltagere eller brugeren, eller for tab af deltagerdata forårsaget af brugerens, webstedets, deltagerens eller tredjeparts forsømmelse.
+		</p>
+		<br>
+		<p>
+			Webstedets ansvar over for brugeren er begrænset til det beløb, brugeren har betalt for ydelser leveret af webstedet.
+		</p>
+		<br>
+		<p>
+			Over for deltagere i betalte webinarer er webstedets ansvar begrænset til det beløb, som brugeren har modtaget.
+		</p>
+		<br>
+		<p>
+			Webstedet garanterer ikke nøjagtigheden, aktualiteten eller fuldstændigheden af de oplysninger, som brugeren formidler i betalte webinarer. Brugeren anerkender, at det indhold eller materiale, der præsenteres under webinaret, kan indeholde fejl eller unøjagtigheder. Webstedet er ikke ansvarligt for sådanne fejl eller for tabt fortjeneste for brugeren eller deltagerne.
+		</p>
+		<br>
+		<p>
+			Webstedet udsteder ikke dokumentation som fakturaer eller kvitteringer til deltagerne. Det er op til brugeren selv at tage hånd om dette, hvis nødvendigt.
+		</p>
+		<br><br>
+		<h3>Tvister</h3>
+		<p>
+			Webstedet bestræber sig på at løse alle henvendelser grundigt, men kan ikke garantere, at enhver sag afgøres til fordel for brugeren eller deltageren. Vi forpligter os dog til at handle hurtigt og gøre vores bedste for at løse enhver situation. I tilfælde af tvister, der kræver involvering af offentlige myndigheder, accepterer både brugeren og deltageren, at gældende EU-lovgivning er gældende.
+		</p>
+		<br><br>
+		<h3>Meddelelser</h3>
+		<p>
+			Send venligst alle henvendelser og tekniske spørgsmål til paid-webinars@myownconference.com. Husk at beskrive dit problem eller tekniske udfordring så detaljeret som muligt.
+		</p>
+		<br><br>
+	',
+	//////////////////////////////////////////////////////////////////////////////////////
+];
